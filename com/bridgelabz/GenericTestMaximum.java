45e25bed61746cc1a9a2b93f1c9ac86aec5262cc
unknown <papisettynagendra>
2022-08-22 11:33:21 +0530

    my first message

---
+++ com/bridgelabz/GenericTestMaximum.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class GenericTestMaximum {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Generic Test Maximum");
+    }
+    }
