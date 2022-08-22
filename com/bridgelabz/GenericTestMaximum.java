e23f2e873ac274da5f45c40754c81e5e61729579
unknown <papisettynagendra>
2022-08-22 12:27:16 +0530

    find max 3 string

--- com/bridgelabz/GenericTestMaximum.java
+++ com/bridgelabz/GenericTestMaximum.java
@@ -1,26 +1,61 @@
 package com.bridgelabz;
 
 public class GenericTestMaximum {
-    public static Float compareTo(Float a, Float b, Float c) {
-        if (a > b) {
-            if (a > c) {
+    public static Integer compareTo(Integer a,Integer b,Integer c) {
+        if(a > b){
+            if(a > c) {
                 return a;
             } else {
                 return c;
             }
         } else {
-            if (b > c) {
+            if(b > c) {
                 return b;
             } else {
                 return c;
             }
         }
     }
+    public static Float compareTo(Float a,Float b,Float c) {
+        if(a > b){
+            if(a > c) {
+                return a;
+            } else {
+                return c;
+            }
+        } else {
+            if(b > c) {
+                return b;
+            } else {
+                return c;
+            }
+        }
+    }
+    public static String compareTo(String str1,String str2,String str3) {
+        if(str1.compareTo(str2) > 0 ) {
+            if(str1.compareTo(str3) > 0) {
+                return str1;
+            } else {
+                return str3;
+            }
+        } else {
+            if(str2.compareTo(str3) > 0) {
+                return str2;
+            } else {
+                return str3;
+            }
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("----!Welcome to Generic Test Maximum!-----");
-        float maxFloat = compareTo(30.10F, 50.12F, 90.52F);
-        System.out.println("Maximum from 3 Float : " + maxFloat);
+        int maxInt=compareTo(50,100,90);
+        System.out.println("Maximum from 3 Integer : "+maxInt);
+        float maxFloat=compareTo(50.12F,20.23F,90.52F);
+        System.out.println("Maximum from 3 Float : "+maxFloat);
+
+        String maxString=compareTo("Apple","Peach","Banana");
+        System.out.println("Maximum from 3 String : "+maxString);
 
     }
 }
